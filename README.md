358c0cfef7a843ed223c8d92a623f169eef1984d
Adroit10 <tripathidevansh7@gmail.com>
2025-02-09 18:44:34 +0530

    Data ingestion notebook added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ### Text-Summmarizer
+
+## Workflows
+
+1. Update config.yaml
+2. Update params.yaml
+3. Update entity
+4. Update configuration manager src- config
+5. Update components
+6. Update the pipeline
+7. update main.py
+8. update app.py
+
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from src.TextSummarization.logging import logger
+from TextSummarization.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from TextSummarization.logging import logger
 
 
-logger.info("Welcome to custom log")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
